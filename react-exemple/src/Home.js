72bb70279b28556397a049f40bd66e2e6c628f78
Amine Mesbahi <mesbahiamine886@gmail.com>
2022-03-13 21:43:49 +0100

    create first conponent page home

--- react-exemple/src/Home.js
+++ react-exemple/src/Home.js
@@ -2,7 +2,8 @@ const Home = () => {
     return ( 
         <div className="Home">
             <h1>Home Page</h1>
-            <h3>content of the home page </h3>
+            <h3>content home </h3>
+            <h3>content of the home page</h3>
         </div>
     );
 }
