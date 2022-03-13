e935072cd674dc25c089ab0a244b6d8ffded52ab
Amine Mesbahi <mesbahiamine886@gmail.com>
2022-03-13 21:40:45 +0100

    create first conponent

---
+++ react-exemple/src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="Home">
+            <h1>Home Page</h1>
+            <h3>content of the home page </h3>
+        </div>
+    );
+}
+
+export default Home;
---
+++ react-exemple/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>WEBSITE LOGO</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create" style={{
+                    textDecoration:'none'
+                }}>new Blog</a>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
